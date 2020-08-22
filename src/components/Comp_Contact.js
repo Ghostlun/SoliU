c99d148e5e63fc46bf49193c11759338c5c9b389
Ghostlun <40644178+Ghostlun@users.noreply.github.com>
2020-08-21 22:14:30 -0500

    Commit for emailjs
    Commit for emailjs

--- src/components/Comp_Contact.js
+++ src/components/Comp_Contact.js
@@ -1,83 +1,115 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import '../asset/css/style.css';
+import * as emailjs from "emailjs-com"
 
 
 
 
 export default class Comp_Contact extends Component {
 
+ constructor(props) {
+
+  super(props)
   
+  this.state = { name : null, phone : null, email :null}
 
-    firstLine = () => {
+ }
+
+handleChange = (e) => {
+
+  this.setState({[e.target.name] : e.target.value})
+  console.log("Check the change", e.target.name)
+}
+
+handleSubmit = (e) => {
+
+  e.preventDefault();
+
+  emailjs.sendForm (
+
+    "gmail",
+    "test",
+    "className",
+    "user_64cUFNeBdOXAD4QZWBXWY"
+  ).then()
+  .catch(); 
+  this.setState({
+    name:"",
+    phone: "",
+    email: ""
+  })
+}
+ 
 
 
-        return  <div>
-        {/* Get data from */}
+
+
+  firstLine = () => {
+
+
+    return <div>
+      {/* Get data from */}
+      <br></br>
+      <div className="CONTACT-US" style={{ marginTop: "50px" }}> CONTACT US </div> <br></br>
+      <form onSubmit={this.handleSubmit}>
+      <p><input type='text' placeholder="first" name="name"  value = {this.state.name} onChange={this.handleChange} /> </p>
+
+      <div className="CONTACT-US"> E-mail </div><br></br>
+      <p><input type='text' placeholder="Email" name="email" size="80" onChange={this.handleChange} value = {this.state.email} /> </p>
+
+
+      <div className="CONTACT-US"> Comment</div><br></br>
+        <p><input type='text' placeholder="Comment" name="phone" size="80" style={{ height: "300" }}  value = {this.state.phone} onChange={this.handleChange} /> </p>
+     
+      </form>
+
+      <button className="submit-icon">Submit</button>
+
+
+    </div>
+  }
+
+  seconeLine = () => {
+
+
+
+
+    return <div>
+      {/* Get data from */}
+      <br></br>
+      <div className="CONTACT-US" style={{ marginTop: "90px" }}> Get in Touch</div> <br></br>
+
+
+      <div className="CONTACT-US"> Address </div><br></br>
+      <div className="info" > School of Communication and Journalism
+      Auburn University
         <br></br>
-        <div className = "CONTACT-US" style = {{marginTop : "50px"}}> CONTACT US </div> <br></br>
-        <form onSubmit = {this.handleSubmit}>
-         <p><input type = 'text' placeholder = "first" name = "first" onChange = {this.handleInputChange}/> </p>
-          </form> 
-       
-        <div className = "CONTACT-US"> E-mail </div><br></br>
-        <form onSubmit = {this.handleSubmit}>
-         <p><input type = 'text' placeholder = "Email" name = "Email" size = "80    " onChange = {this.handleInputChange}/> </p>
-        </form> 
-
-
-        <div className = "CONTACT-US"> Comment</div><br></br>
-        <form onSubmit = {this.handleSubmit}>
-        <p><input type = 'text' placeholder = "Comment" name = "Comment" size = "80" style ={{height :   "300"}} onChange = {this.handleInputChange}/> </p>
-        </form> 
-    
-        <button className = "submit-icon">Submit</button>
-
-        
-
-  </div>
-    }
-
-    seconeLine = () => {
-
-         
-
-
-        return  <div>
-        {/* Get data from */}
-        <br></br>
-        <div className = "CONTACT-US" style = {{marginTop : "90px"}}> Get in Touch</div> <br></br>
-      
-       
-        <div className = "CONTACT-US"> Address </div><br></br>
-         <div className = "info" > School of Communication and Journalism 
-             Auburn University
-        <br></br>
         <br></br>
 
-237 Tichenor Hall 
-Auburn AL 36849-5206</div>
+        237 Tichenor Hall
+        Auburn AL 36849-5206</div>
 
-        <div className = "CONTACT-US"> Join the team</div><br></br>
-        <p> Dr. Myoung-Gi Chon <br></br>
-Tel: 33-844-2721 <br></br>
-mzc0113@auburn.edu
-</p>
-    
-
-        
-
-  </div>
-    }
-   
-    
-
-    render() {
-
-        return (
+      <div className="CONTACT-US"> Join the team</div><br></br>
+      <p> Dr. Myoung-Gi Chon <br></br>
+      Tel: 33-844-2721 <br></br>
+      mzc0113@auburn.edu
+      </p>
 
 
-        <div
+
+
+    </div>
+  }
+
+
+
+  render() {
+
+    return (
+
+
+      <div
         style={{
           display: 'grid',
           gridTemplateColumns: 'repeat(3, 1fr)',
@@ -93,12 +125,12 @@ mzc0113@auburn.edu
             alignItems: 'center'
           }}
         >
-            {this.firstLine()}      
-          </div>
+          {this.firstLine()}
+        </div>
 
 
-        
-          <div
+
+        <div
           style={{
             display: 'flex',
             justifyContent: 'center',
@@ -106,11 +138,11 @@ mzc0113@auburn.edu
           }}
         >
         </div>
-          {this.seconeLine()}
-          </div>
-        );
+        {this.seconeLine()}
+      </div>
+    );
 
-    }
+  }
 
-    
+
 }
