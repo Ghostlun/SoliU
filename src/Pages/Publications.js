df779396eac1fcad39486e941f2051c1e09fb45a
Ghostlun <40644178+Ghostlun@users.noreply.github.com>
2020-08-21 20:29:09 -0500

    publication
    publication

--- src/Pages/Publications.js
+++ src/Pages/Publications.js
@@ -1,63 +1,120 @@
 import React from 'react';
 import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
+import '../asset/css/style.css'
+
 const Publications = () => {
   return (
+    <div className = "info"> 
+    
     <div>
-      Chon, M. & Park, H. (2020). Social media activism in the digital age:
-      Testing an integrative model of activism on contentious issues. Journalism
-      & Mass Communication Quarterly 97(1)
-      72-97. https://doi.org/10.1177/1077699019835896 Chon, M. Lee, H., & J.-N.,
-      Kim. (2020). Values of government public relations for a rocky road to
-      participatory democracy: Testing public engagement, empowerment, and
-      serenity hypotheses in public sector communication. Partecipazione e
-      Conflitto [Participation and Conflict], 13(2)
-      2020:1110-1131. http://siba-ese.unisalento.it/index.php/paco/article/view/22504
-      Chon, M. (2019). Government public relations when trouble hits: exploring
-      political dispositions, situational variables, and government–public
-      relationships to predict communicative action of publics. Asian Journal of
-      Communication, 29(5), 424-440, DOI: 10.1080/01292986.2019.1649438 Chon,
-      M. & Park, H. (2019). Predicting public support for government actions in
-      a public health crisis: Testing fear, positive organization-public
-      relationship, and behavioral intention in the framework of the situational
-      theory of problem solving. Health Communication. Online
-      First. https://doi.org/10.1080/10410236.2019.1700439 Chon, M. & Fondren,
-      E. (2019). Seeing a crisis through colored glasses: How partisan media
-      leads to attribution of crisis responsibility and government
-      trust. Journal of Public Affairs, 19(4),
-      1-9.  https://doi.org/10.1002/pa.1950 Jeong, J., Kim, Y., & Chon,
-      M. (2018). The importance of online communication behaviors for coping
-      processes and outcomes of dementia patients and their caregivers. Health
-      Communication, 33:1, 5-13. https://doi.org/10.1080/10410236.2016.1242030
-      Chon, M. & Park, H. (2017). All does not fit all: Health audience
-      segmentation and prediction of health behaviors in cancer
-      prevention. Health Marketing Quarterly, 34, pp.
-      202-216. http://dx.doi.org/10.1080/07359683.2017.1346434  Chon, M. & Kim,
-      J.-N. (2016). Understanding active publics and their communicative action
-      through public segmentation: Applying situational theory of problem
-      solving to public segmentation in an organizational crisis
-      situation. Journal of Public Relations, 20(3), 113-138.* Park, H., Reber,
-      B., & Chon, M. (2016). Tweeting as Health communication: Health
-      organizations' use of Twitter for health promotion and public
-      engagement. Journal of Health Communication, 21(2), 188-198. Kim, Y.,
-      Miller, A., & Chon, M. (2016). Communicating with key publics in crisis
-      communication: The synthetic approach to the public segmentation in CAPS
-      (Communicative Action in Problem Solving). Journal of Contingencies and
-      Crisis Management, 24(2), 82-94. Kim, Y., Chon, M., & Miller, A. (2015).
-      Cross-national ongoing crisis communication on social media: A comparative
-      analysis of Twitter regarding Asiana Airlines crash crisis in South Korea
-      and US. Asian Communication Research, 11(1), 22-51.  Book Chapter and
-      Invited Articles J.-N. Kim, L. Tam, & Chon, M. (Accepted). Conceptual
-      genealogy of the situational theory of problem solving: Theorizing
-      communication for strategic behavioral communication management. In
-      Chiara, Valentini (Ed.), Handbook of Public Relations. Lee, Y., Chon,
-      M., Oh Y., & Kim, J. (2017). Understanding communicative activism of
-      publics in digital networked society: A taxonomy of digitalized
-      communicative actions. In Sandra C. Duhé (Ed.), New Media and Public
-      Relations (3rd ed.), Peter Lang. Chon, M. & Park, S. (May Issue 2, 2019).
-      Employee communication behaviors and scouting: Two pillars to build
-      sustainable innovation. Dong-A Business Review. 우호적커뮤니케이션과
-      스카우팅: 지속가능한 혁신을 위한 두 가지 축
-      https://dbr.donga.com/article/view/1101/article_no/9139 *Korean Journal   
+
+    <br></br>
+    <br></br>
+
+    <br></br>
+
+    <br></br>
+    <div>
+    <h3><b>Publications</b></h3>
+  </div>
+    <div style = {{marginLeft : 10}}> <pre></pre>His primary research interests lie in Risk-Crisis Management, Strategic Communication, Health Communication, and Public Diplomacy. His research interests have been developed by understanding how issues are presented in media and communicative behaviors of publics to solve public relations and health issues. He focuses on issues and crisis communication, organizational-public relationship (OPR), employee communication behavior (ECB), and social activism in the networked society. He also expands his research interest to public diplomacy by exploring how public relations theories can be applied into public diplomacy issues. 
+   
+    </div>
+
+    <br></br>
+    <br></br>
+    <br></br>
+
+    </div>
+
+
+
+
+<h4><b>Journal Articles</b></h4>
+    <div style = {{margin : 10  
+}}>
+  <div>Chon, M. & Park, H. (2020). Social media activism in the digital age: Testing an integrative model of activism on contentious issues. Journalism & Mass Communication Quarterly 97(1) 72-97.
+  <a href=" https://doi.org/10.1177/1077699019835896"style ={{marginLeft : 10}}>link</a> </div>
+  <br></br>
+
+  <div>Chon, M. Lee, H., & J.-N., Kim. (2020). Values of government public relations for a rocky road to participatory democracy: Testing public engagement, empowerment, and serenity hypotheses in public sector communication. Partecipazione e Conflitto [Participation and Conflict], 13(2) 2020:1110-1131.
+  <a href=" http://siba-ese.unisalento.it/index.php/paco/article/view/22504" style ={{marginLeft : 10}}>link</a> </div>
+    <br></br>
+
+
+  <div>Chon, M. (2019). Government public relations when trouble hits: exploring political dispositions, situational variables, and government–public relationships to predict communicative action of publics. Asian Journal of Communication, 29(5), 424-440,
+  <a href="  10.1080/01292986.2019.1649438" style ={{marginLeft : 10}} >link</a> </div>
+  <br></br>
+
+  <div>Chon, M. & Park, H. (2019). Predicting public support for government actions in a public health crisis: Testing fear, positive organization-public relationship, and behavioral intention in the framework of the situational theory of problem solving. Health Communication. Online First.
+  <a href="https://doi.org/10.1080/10410236.2019.1700439" style ={{marginLeft : 10}}>link</a> </div>
+  <br></br>
+
+  <div>Chon, M. & Fondren, E. (2019). Seeing a crisis through colored glasses: How partisan media leads to attribution of crisis responsibility and government trust. Journal of Public Affairs, 19(4), 1-9.
+  <a href="  https://doi.org/10.1002/pa.1950 " style ={{marginLeft : 10}} >link</a> </div>
+  <br></br>
+
+  <div>Jeong, J., Kim, Y., & Chon, M. (2018). The importance of online communication behaviors for coping processes and outcomes of dementia patients and their caregivers. Health Communication, 33:1, 5-13. 
+  <a href=" http://dx.doi.org/10.1080/07359683.2017.1346434 " style ={{marginLeft : 10}}>link</a> </div>
+  <br></br>
+
+
+  <div>Chon, M. & Kim, J.-N. (2016). Understanding active publics and their communicative action through public segmentation: Applying situational theory of problem solving to public segmentation in an organizational crisis situation. Journal of Public Relations, 20(3), 113-138.*
+  <a href=" http://dx.doi.org/10.1080/07359683.2017.1346434 " style ={{marginLeft : 10}}>link</a> </div>
+  <br></br>
+
+
+  <div>Park, H., Reber, B., & Chon, M. (2016). Tweeting as Health communication: Health organizations' use of Twitter for health promotion and public engagement. Journal of Health Communication, 21(2), 188-198.
+  </div>
+  <br></br>
+
+  <div>Kim, Y., Miller, A., & Chon, M. (2016). Communicating with key publics in crisis communication: The synthetic approach to the public segmentation in CAPS (Communicative Action in Problem Solving). Journal of Contingencies and Crisis Management, 24(2), 82-94.  
+  </div>
+  <br></br>
+
+
+  <div>Kim, Y., Chon, M., & Miller, A. (2015). Cross-national ongoing crisis communication on social media: A comparative analysis of Twitter regarding Asiana Airlines crash crisis in South Korea and US. Asian Communication Research, 11(1), 22-51. 
+  </div>
+  <br></br>
+  </div>
+
+
+<div>
+<br></br>
+<h4><b>Book Chapter and Invited Articles</b></h4>
+<br></br>
+</div>
+
+<div>J.-N. Kim, L. Tam, & Chon, M. (Accepted). Conceptual genealogy of the situational theory of problem solving: Theorizing communication for strategic behavioral communication management. In Chiara, Valentini (Ed.), Handbook of Public Relations.</div>
+<br></br>
+
+<div>Lee, Y., Chon, M., Oh Y., & Kim, J. (2017). Understanding communicative activism of publics in digital networked society: A taxonomy of digitalized communicative actions. In Sandra C. Duhé (Ed.), New Media and Public Relations (3rd ed.), Peter Lang.</div>
+<br></br>
+
+<div>Chon, M. & Park, S. (May Issue 2, 2019). Employee communication behaviors and scouting: Two pillars to build sustainable innovation. Dong-A Business Review. 우호적커뮤니케이션과 스카우팅: 지속가능한 혁신을 위한 두 가지 축</div>
+
+
+<br></br>
+<div> 
+<a href=" https://dbr.donga.com/article/view/1101/article_no/9139" style = {{fontSize : 10}}>https://dbr.donga.com/article/view/1101/article_no/9139
+</a> </div>
+
+
+
+<br></br>
+
+<div>*Korean Journal</div>
+
+
+
+
+
+<div>
+
+
+
+    </div>
        
       <Footer />
     </div>
