36acc5bbeb98de4ba11b4d0b1e9360395632ab7b
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2020-09-18 01:53:19 -0500

    commit for jungpyo hong final

--- src/components/IntrodoingMember/Intro_jungpyo.js
+++ src/components/IntrodoingMember/Intro_jungpyo.js
@@ -4,13 +4,17 @@ import LinkIn from '../../asset/image/linkedin.png';
 const Intro_jungpyo = () => {
   return (
     <div>
-      <h4>Hello this is Jungpyo Hong to meet you</h4>
+      <h4>Hello this is Jungpyo Hong Nice to meet you</h4>
 
-      <h5>If you like more information about me, please click to</h5>
+      <h5>
+        If you like more information about me, please click to LinkedIn and
+        Github
+      </h5>
       <div>
         <a href='https://www.linkedin.com/in/jungpyo-hong-2987891a1/?locale=en_US'>
           <img src={LinkIn} alt='LinkIn' style={{ width: '100px' }} />
         </a>
+
         <a href='https://github.com/jphong1111'>
           <img src={GitHub} alt='github' style={{ width: '100px' }} />
         </a>
