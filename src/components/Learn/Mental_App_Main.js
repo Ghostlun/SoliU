db8a0927259fd4bc8c8c37be8f689a8d80c972b7
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2020-10-15 17:22:26 -0500

    commit#1015 change intro style of mental app

--- src/components/Learn/Mental_App_Main.js
+++ src/components/Learn/Mental_App_Main.js
@@ -5,33 +5,23 @@ import Brain from './Mental_App_Brain';
 import '../../asset/css/style.css';
 
 const Mental_App_Main = () => {
-
   // Function Add 가능한 부분
-  // 
-  // 
-  // 
-  // 
+  //
+  //
+  //
+  //
 
   return (
-
     // Center 안의 부분은 Mental app 관련 전체부분//
     <center>
-
-
-
-      <div>
-         {/* 사진 디스플레이 되는 부분  */}
-
-
-
-      </div>
+      <div>{/* 사진 디스플레이 되는 부분  */}</div>
       <div className='container_for_Mental_Main'>
         <div>
           <h1
             style={{
               display: 'flex',
               justifyContent: 'center',
-              alignItems: 'center'
+              alignItems: 'center',
             }}
           >
             How Are U?
@@ -42,29 +32,33 @@ const Mental_App_Main = () => {
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
-            marginTop: '50px'
+            marginTop: '50px',
           }}
         >
           <Brain />
         </div>
-        <div className='btn-grid'>
+        <div style={{ marginTop: '30px' }}>
           <button className='btn-mental-main'>
             <Link to='/research/mental_app/information'>
               <text style={{ color: 'white' }}>LEARN</text>
             </Link>
           </button>
-
+        </div>
+        <div style={{ marginTop: '30px' }}>
           <button className='btn-mental-main'>
             <Link to='/research/mental_app/self_test'>
               <text style={{ color: 'white' }}>SELF TEST</text>
             </Link>
           </button>
-
+        </div>
+        <div style={{ marginTop: '30px' }}>
           <button className='btn-mental-main'>
             <Link to='/research/mental_app/chat'>
               <text style={{ color: 'white' }}>CHAT</text>
             </Link>
           </button>
+        </div>
+        <div style={{ marginTop: '30px' }}>
           <button className='btn-mental-main'>
             <Link to='/research/mental_app/resource_center'>
               <text style={{ color: 'white' }}>RESOURCE CENTER</text>
