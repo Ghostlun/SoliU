5adaa3ec7c76a99161af0183bc025abb2b988a7c
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2020-10-22 16:47:12 -0500

    commit#1022 change app intro

--- src/components/Learn/Mental_App_MainOptionScreen.js
+++ src/components/Learn/Mental_App_MainOptionScreen.js
@@ -1,51 +1,59 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import BrainImg from '../../asset/image/brain.png';
 import { Link } from 'react-router-dom';
 
 const MainOptionScreen = () => {
-    return(
-        <Fragment>
-          <center>
-            <div>
-              <h1 style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center'
-                }}>
-              How Are U?
-              </h1>
-            </div>
+  return (
+    <Fragment>
+      <center>
+        <div>
+          <h1
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            How Are U?
+          </h1>
+        </div>
 
-            <div>
-              <img src={BrainImg} alt='brain'
-                style={{
-                  width: '300px',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginTop: '50px'
-                }} />
-            </div>
+        <div>
+          <img
+            src={BrainImg}
+            alt='brain'
+            style={{
+              width: '300px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: '50px',
+            }}
+          />
+        </div>
 
-        <div className='btn-grid'>
+        <div style={{ marginTop: '30px' }}>
           <button className='btn-mental-main'>
             <Link to='/research/mental_app/information'>
               <text style={{ color: 'white' }}>LEARN</text>
             </Link>
           </button>
-
+        </div>
+        <div style={{ marginTop: '30px' }}>
           <button className='btn-mental-main'>
             <Link to='/research/mental_app/self_test'>
               <text style={{ color: 'white' }}>SELF TEST</text>
             </Link>
           </button>
-
+        </div>
+        <div style={{ marginTop: '30px' }}>
           <button className='btn-mental-main'>
             <Link to='/research/mental_app/chat'>
               <text style={{ color: 'white' }}>CHAT</text>
             </Link>
-          </button> 
-          
+          </button>
+        </div>
+        <div style={{ marginTop: '30px' }}>
           <button className='btn-mental-main'>
             <Link to='/research/mental_app/resource_center'>
               <text style={{ color: 'white' }}>RESOURCE CENTER</text>
