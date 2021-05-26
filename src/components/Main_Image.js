0b469266882c5f1aae356c5e2d8cc035e345b75d
Ghostlun <40644178+Ghostlun@users.noreply.github.com>
2021-05-25 22:36:18 -0500

    Image update
    Image update

--- src/components/Main_Image.js
+++ src/components/Main_Image.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Pic from '../asset/image/soliU_Team.png';
+import Pic from '../asset/image/MainImage1.jpeg';
 const Main_Image = () => {
   return (
     <div>
-      <img src={Pic} alt='main-pic-img-set' width="auto" height="407.35px" 
+      <cetner>
+      <img src={Pic} alt='main-pic-img-set' width="1200px" height = "500px"
       />
-
+      </cetner>
     </div>
   );
 };
